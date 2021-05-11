cbbbb911d3765812d5c7b44a4e9fedb517ca6e38
Gouaby Kalivogui <gouabyk14@yahoo.fr>
2021-05-11 15:37:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -22,42 +22,38 @@ const App = () => {
     // return true;
   }
 
-// When the data is already set as an option to the contract itself
-  // myContract.options.data = '0x12345...';
-
   const deployContract = async () => {
     const accounts = await window.web3.eth.getAccounts();
     const account1 = accounts[0];
 
-    const myContract = new window.web3.eth.Contract(abi, {gasPrice: 52000000, from: account1});
-
-
-    myContract.deploy({
-      data: bytecode
-    })
-    .send({
+    const myContract = new window.web3.eth.Contract(abi, {
+      gasPrice: 52000000,
       from: account1,
-      gas: 7492052,
-      gasPrice: 52000000
-    })
-    .then(function(newContractInstance){
-      console.log(newContractInstance.options.address) // instance with the new contract address
     });
-  }
-  
 
+    myContract
+      .deploy({
+        data: bytecode,
+      })
+      .send({
+        from: account1,
+        gas: 7492052,
+        gasPrice: 52000000,
+      })
+      .then(function (newContractInstance) {
+        console.log(newContractInstance.options.address); // instance with the new contract address
+      });
+  };
 
   const getMyBalance = async () => {
     const accounts = await window.web3.eth.getAccounts();
-    window.web3.eth.getBalance(accounts[0],
-      function (err, result) {
-        if (err) {
-          console.log(err);
-        } else {
-          setBalance(window.web3.utils.fromWei(result, "ether") + " ETH");
-        }
+    window.web3.eth.getBalance(accounts[0], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        setBalance(window.web3.utils.fromWei(result, "ether") + " ETH");
       }
-    );
+    });
   };
   getMyBalance();
 
@@ -71,9 +67,9 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <h4>Account-0: {address}</h4>
-      <h3>Balance: {balance}</h3>
-      <button onClick={deployContract}>Deploy a contract</button>
+        <h4>Account-0: {address}</h4>
+        <h3>Balance: {balance}</h3>
+        <button onClick={deployContract}>Deploy a contract</button>
       </header>
     </div>
   );
